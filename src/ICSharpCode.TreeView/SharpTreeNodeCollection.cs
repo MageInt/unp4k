a1339575d1ba9f90ccfaddc47906c6a69ec05c50
MageInt <141229711+MageInt@users.noreply.github.com>
2023-10-24 00:06:22 +0200

    update SharpTreeNodeCollection from decompiled DLL

--- src/ICSharpCode.TreeView/SharpTreeNodeCollection.cs
+++ src/ICSharpCode.TreeView/SharpTreeNodeCollection.cs
@@ -6,83 +6,97 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Collections.Specialized;
+using System.Collections;
 
 namespace ICSharpCode.TreeView
 {
-	/// <summary>
-	/// Collection that validates that inserted nodes do not have another parent.
-	/// </summary>
-	public sealed class SharpTreeNodeCollection : IList<SharpTreeNode>, INotifyCollectionChanged
+	public sealed class SharpTreeNodeCollection : IList<SharpTreeNode>, ICollection<SharpTreeNode>, IEnumerable<SharpTreeNode>, IEnumerable, INotifyCollectionChanged
 	{
-		readonly SharpTreeNode parent;
-		List<SharpTreeNode> list = new List<SharpTreeNode>();
-		bool isRaisingEvent;
-		
+		private readonly SharpTreeNode parent;
+
+		private List<SharpTreeNode> list = new List<SharpTreeNode>();
+
+		private bool isRaisingEvent;
+
+		public SharpTreeNode this[int index]
+		{
+			get
+			{
+				return list[index];
+			}
+			set
+			{
+				ThrowOnReentrancy();
+				SharpTreeNode sharpTreeNode = list[index];
+				if (sharpTreeNode != value)
+				{
+					ThrowIfValueIsNullOrHasParent(value);
+					list[index] = value;
+					OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, value, sharpTreeNode, index));
+				}
+			}
+		}
+
+		public int Count => list.Count;
+
+		bool ICollection<SharpTreeNode>.IsReadOnly => false;
+
+		public event NotifyCollectionChangedEventHandler CollectionChanged;
+
 		public SharpTreeNodeCollection(SharpTreeNode parent)
 		{
 			this.parent = parent;
 		}
-		
-		public event NotifyCollectionChangedEventHandler CollectionChanged;
-		
-		void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+
+		private void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
 		{
 			Debug.Assert(!isRaisingEvent);
 			isRaisingEvent = true;
-			try {
+			try
+			{
 				parent.OnChildrenChanged(e);
-				if (CollectionChanged != null)
-					CollectionChanged(this, e);
-			} finally {
+				if (this.CollectionChanged != null)
+				{
+					this.CollectionChanged(this, e);
+				}
+			}
+			finally
+			{
 				isRaisingEvent = false;
 			}
 		}
-		
-		void ThrowOnReentrancy()
+
+		private void ThrowOnReentrancy()
 		{
 			if (isRaisingEvent)
+			{
 				throw new InvalidOperationException();
+			}
 		}
-		
-		void ThrowIfValueIsNullOrHasParent(SharpTreeNode node)
+
+		private void ThrowIfValueIsNullOrHasParent(SharpTreeNode node)
 		{
 			if (node == null)
+			{
 				throw new ArgumentNullException("node");
+			}
+
 			if (node.modelParent != null)
+			{
 				throw new ArgumentException("The node already has a parent", "node");
-		}
-		
-		public SharpTreeNode this[int index] {
-			get {
-				return list[index];
-			}
-			set {
-				ThrowOnReentrancy();
-				var oldItem = list[index];
-				if (oldItem == value)
-					return;
-				ThrowIfValueIsNullOrHasParent(value);
-				list[index] = value;
-				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, value, oldItem, index));
 			}
 		}
-		
-		public int Count {
-			get { return list.Count; }
-		}
-		
-		bool ICollection<SharpTreeNode>.IsReadOnly {
-			get { return false; }
-		}
-		
+
 		public int IndexOf(SharpTreeNode node)
 		{
 			if (node == null || node.modelParent != parent)
+			{
 				return -1;
-			else
-				return list.IndexOf(node);
+			}
+
+			return list.IndexOf(node);
 		}
-		
+
 		public void Insert(int index, SharpTreeNode node)
 		{
 			ThrowOnReentrancy();
@@ -90,40 +104,49 @@ namespace ICSharpCode.TreeView
 			list.Insert(index, node);
 			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, node, index));
 		}
-		
+
 		public void InsertRange(int index, IEnumerable<SharpTreeNode> nodes)
 		{
 			if (nodes == null)
+			{
 				throw new ArgumentNullException("nodes");
-			ThrowOnReentrancy();
-			List<SharpTreeNode> newNodes = nodes.ToList();
-			if (newNodes.Count == 0)
-				return;
-			foreach (SharpTreeNode node in newNodes) {
-				ThrowIfValueIsNullOrHasParent(node);
 			}
-			list.InsertRange(index, newNodes);
-			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, newNodes, index));
+
+			ThrowOnReentrancy();
+			List<SharpTreeNode> list = nodes.ToList();
+			if (list.Count == 0)
+			{
+				return;
+			}
+
+			foreach (SharpTreeNode item in list)
+			{
+				ThrowIfValueIsNullOrHasParent(item);
+			}
+
+			this.list.InsertRange(index, list);
+			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, list, index));
 		}
-		
+
 		public void RemoveAt(int index)
 		{
 			ThrowOnReentrancy();
-			var oldItem = list[index];
+			SharpTreeNode changedItem = list[index];
 			list.RemoveAt(index);
-			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, oldItem, index));
+			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, changedItem, index));
 		}
-		
+
 		public void RemoveRange(int index, int count)
 		{
 			ThrowOnReentrancy();
-			if (count == 0)
-				return;
-			var oldItems = list.GetRange(index, count);
-			list.RemoveRange(index, count);
-			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, oldItems, index));
+			if (count != 0)
+			{
+				List<SharpTreeNode> range = list.GetRange(index, count);
+				list.RemoveRange(index, count);
+				OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, range, index));
+			}
 		}
-		
+
 		public void Add(SharpTreeNode node)
 		{
 			ThrowOnReentrancy();
@@ -131,77 +154,103 @@ namespace ICSharpCode.TreeView
 			list.Add(node);
 			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, node, list.Count - 1));
 		}
-		
+
 		public void AddRange(IEnumerable<SharpTreeNode> nodes)
 		{
-			InsertRange(this.Count, nodes);
+			InsertRange(Count, nodes);
 		}
-		
+
+		public void Sort(Comparison<SharpTreeNode> comparison)
+		{
+			list.Sort(comparison);
+		}
+
+		public void Sort(IComparer<SharpTreeNode> comparer)
+		{
+			list.Sort(comparer);
+		}
+
 		public void Clear()
 		{
 			ThrowOnReentrancy();
-			var oldList = list;
+			List<SharpTreeNode> changedItems = list;
 			list = new List<SharpTreeNode>();
-			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, oldList, 0));
+			OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, changedItems, 0));
 		}
-		
+
 		public bool Contains(SharpTreeNode node)
 		{
 			return IndexOf(node) >= 0;
 		}
-		
+
 		public void CopyTo(SharpTreeNode[] array, int arrayIndex)
 		{
 			list.CopyTo(array, arrayIndex);
 		}
-		
+
 		public bool Remove(SharpTreeNode item)
 		{
-			int pos = IndexOf(item);
-			if (pos >= 0) {
-				RemoveAt(pos);
+			int num = IndexOf(item);
+			if (num >= 0)
+			{
+				RemoveAt(num);
 				return true;
-			} else {
-				return false;
 			}
+
+			return false;
 		}
-		
+
 		public IEnumerator<SharpTreeNode> GetEnumerator()
 		{
 			return list.GetEnumerator();
 		}
-		
-		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+
+		IEnumerator IEnumerable.GetEnumerator()
 		{
 			return list.GetEnumerator();
 		}
-		
+
 		public void RemoveAll(Predicate<SharpTreeNode> match)
 		{
 			if (match == null)
+			{
 				throw new ArgumentNullException("match");
+			}
+
 			ThrowOnReentrancy();
-			int firstToRemove = 0;
-			for (int i = 0; i < list.Count; i++) {
-				bool removeNode;
+			int num = 0;
+			for (int i = 0; i < list.Count; i++)
+			{
 				isRaisingEvent = true;
-				try {
-					removeNode = match(list[i]);
-				} finally {
+				bool flag;
+				try
+				{
+					flag = match(list[i]);
+				}
+				finally
+				{
 					isRaisingEvent = false;
 				}
-				if (!removeNode) {
-					if (firstToRemove < i) {
-						RemoveRange(firstToRemove, i - firstToRemove);
-						i = firstToRemove - 1;
-					} else {
-						firstToRemove = i + 1;
+
+				if (!flag)
+				{
+					if (num < i)
+					{
+						RemoveRange(num, i - num);
+						i = num - 1;
 					}
-					Debug.Assert(firstToRemove == i + 1);
+					else
+					{
+						num = i + 1;
+					}
+
+					Debug.Assert(num == i + 1);
 				}
 			}
-			if (firstToRemove < list.Count) {
-				RemoveRange(firstToRemove, list.Count - firstToRemove);
+
+			if (num < list.Count)
+			{
+				RemoveRange(num, list.Count - num);
 			}
 		}
 	}
